7707544f0e9e8b5fbf997c91a0aec239548db13d
David <zhangbiao001@chinasofti.com>
2019-03-02 17:36:52 +0800

    Update

---
+++ amoudle/doc/README.md
@@ -0,0 +1,2 @@
+
+new moudle -> android library
---
+++ amoudle/src/main/java/com/wwe/amoudle/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.wwe.amoudle
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @name ComponentApp
+ * @class name：com.wwe.amoudle
+ * @class describe
+ * @anthor David
+ * @time 2019/3/2 1:58 PM
+ * @class describe
+ */
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_layout)
+    }
+}
